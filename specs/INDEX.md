601394d4c3bd080997c8e9635e8e6628d24cd857
akai-inu <akai_inu@live.jp>
2017-07-20 00:41:35 +0900

    add Specs Initial Draft

---
+++ specs/INDEX.md
@@ -0,0 +1,5 @@
+# 仕様
+
+- [概要](./INTRODUCTION.md)
+- [エンティティ](./ENTITIES.md)
+- [メモ](./MEMOS.md)
--- src/client/Game.ts
+++ src/client/Game.ts
@@ -1,18 +1,30 @@
 import * as Phaser from 'phaser';
 
-export default class {
-  game: Phaser.Game;
+let game: Phaser.Game;
 
+export default class {
   constructor() {
-    this.game = new Phaser.Game(854, 480, Phaser.AUTO, "content", this);
+    game = new Phaser.Game(800, 600, Phaser.AUTO, null, this, false, false, null);
   }
 
   preload() {
+    game.load.spritesheet('player', 'assets/player.png', 32, 32);
   }
 
   create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    const player = game.add.sprite(128, 128, 'player', 0);
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
   }
 
-  update() {
+  /**
+   * ゲームインスタンスを取得する
+   *
+   * @return Phaser.Game
+   */
+  static getInstance(): Phaser.Game {
+    return game;
   }
 }
